5c5b2a8bc87836cbef307afc400d758c888c99f6
Ramiro Gómez <code@ramiro.org>
2024-11-04 22:04:46 +0100

    new deployment

--- compiled/comparison.js
+++ compiled/comparison.js
@@ -1,3 +1,45 @@
 "use strict";
 
-comparison.js;
+function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t["return"] || t["return"](); } finally { if (u) throw o; } } }; }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+var slugs;
+if (slugs = document.location.search.slice(1).split('-vs-')) {
+  fetch('/data/chromebook_specs.json').then;
+  fetch('/data/chromebook_specs.json').then(function (resp) {
+    return resp.json();
+  }).then(function (data) {
+    var specs = [];
+    var _iterator = _createForOfIteratorHelper(data),
+      _step;
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var spec = _step.value;
+        if (slugs.includes(spec.url.replaceAll('/', ''))) {
+          specs.push(spec);
+        }
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+    specs && show(specs);
+  });
+}
+function show(specs) {
+  var thead = document.getElementsByTagName('thead')[0];
+  var tr = thead.appendChild(document.createElement('tr'));
+  var _iterator2 = _createForOfIteratorHelper(specs),
+    _step2;
+  try {
+    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+      var spec = _step2.value;
+      tr.appendChild(document.createElement('th')).textContent = spec.title;
+    }
+  } catch (err) {
+    _iterator2.e(err);
+  } finally {
+    _iterator2.f();
+  }
+}
